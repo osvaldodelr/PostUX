81b018a239c46de8a8690688a82e790bc6dba528
osvaldodelr <ing.osvaldodelrosario@gmail.com>
2019-07-12 00:16:25 -0400

    update of the login and its designs

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/login', (req, res) => {
-    res.send('hello login');
+    res.render('./login/login');
 });
 
 module.exports = router;
